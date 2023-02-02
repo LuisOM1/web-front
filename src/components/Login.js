949c27b1718adfbceadfc8e7d0f1a825750895b6
LUIS <123>
2023-02-02 13:02:52 -0500

    login-responsive visible

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,14 @@
 import '../css/login.css';
 import  logo from  '../img/logo.png';
-import Wsp from './Wsp';
 
 function Login() {
   return (
-    <div>
-        < Wsp/>
-        <div className='login'>
+
+    <div className='login'>
+        <div className='login_sup'>
+            <img src={logo} alt="GpsSkies"></img>
+        </div>
             <div className='login_container_left'>
-                <div className='login_sup'>
-                    <img src={logo} alt="GpsSkies"></img>
-                </div>
                 <div className='login_center'>
                     <h5>¿No tienes una cuenta?</h5>
                     <button>
@@ -20,26 +18,28 @@ function Login() {
             </div>
 
             <div className='login_container_right'>
-                <form className='login_form'>
-                    <h5>Iniciar Sesión</h5>
-                    <div className='div_input'>
-                        <input aria-invalid="false" placeholder="Email" required="" type="email"></input>
-                    </div>
-                    <div className='div_input'>
-                        <input aria-invalid="false" placeholder="Contraseña" type="password"></input>
-                    </div>
-                    <div>
-                        <button type="submit">
-                            <span>Ingresa</span>
-                        </button>
-                    </div>
-                    <div className='login_help'>
-                        <a>¿Olvidaste tu contraseña?</a>
-                    </div> 
-                </form>
+                <div className='login_form'>
+                    <form>
+                        <h5>Iniciar Sesión</h5>
+                        <div className='div_input'>
+                            <input aria-invalid="false" placeholder="Email" required="" type="email"></input>
+                        </div>
+                        <div className='div_input'>
+                            <input aria-invalid="false" placeholder="Contraseña" type="password"></input>
+                        </div>
+                        <div>
+                            <button type="submit">
+                                <span>Ingresa</span>
+                            </button>
+                        </div>
+                        <div className='login_help'>
+                            <a>¿Olvidaste tu contraseña?</a>
+                        </div> 
+                    </form>
+                </div>
             </div>
-        </div>
     </div>
+
   );
 }
 
