c90d7990c4c02f7681d2a20851fb9cc96192c2dc
LUIS <123>
2023-02-08 13:02:09 -0500

    section-about corregido

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ import GoTop from './GoTop';
 import CountUp from './CountUp';
 import Wsp from './Wsp';
 import SliderNav from './SliderNav';
-
+import { Link } from 'react-router-dom';
 
 //Componente funcional About
 function About (){
@@ -20,7 +20,7 @@ function About (){
             < div className='about'>
             	<div className='about_container'>
 					<div className='about_container_1'>
-						<div className='about_text'>
+						<div className='about_container_left'>
 							<span className="about_sub_title">Sobre Nosotros</span>
 							<h2>Brindamos servicios de calidad <br></br>
 								<span>desde 2015</span>
@@ -36,7 +36,7 @@ function About (){
 									<div className='icon_objective'>
 										<i className="fa-solid fa-bullseye"></i>
 									</div>
-									<div className='about_mision_text'>
+									<div className='about_objective_text'>
 										<h3>Mision</h3>
 										<p>Ofrecer soluciones tecnológicas y humanas, que nos permitan ofrecer 
 											información más precisa, completa, confiable y en tiempo real que optimicen 
@@ -51,7 +51,7 @@ function About (){
 									<div className='icon_objective'>
 										<i className="fa-solid fa-bullseye"></i>
 									</div>
-									<div className='about_vision_text'>
+									<div className='about_objective_text'>
 										<h3>Vision</h3>
 										<p>Ser líderes en soluciones tecnológicas Innovadoras , para el transporte 
 											vehicular Seguridad Electronica logrando ser reconocidos a nivel nacional 
@@ -63,7 +63,7 @@ function About (){
 							</div>
 						</div>
 
-						<div className='about_img'>
+						<div className='about_container_right'>
 							<img src={img_about} alt="David De La Cruz"></img>
 						</div>
 					</div>
@@ -113,18 +113,14 @@ function About (){
 					</div>
 				</div>
 
+
 				<div className="about_container_3">
 					<div className="about_video">
-						<iframe 
-							width="560" 
-							height="315" 
-							src="https://www.youtube.com/embed/l0rau5crOls" 
-							title="YouTube video player" 
-							frameBorder="0"
-							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-							allowFullScreen>
-						</iframe>
-						
+						<span className="video_btn_border1"></span>
+						<span className="video_btn_border2"></span>
+						<a href="https://www.youtube.com/watch?v=l0rau5crOls" target="_blank" className='about_video_icon' rel="noreferrer">
+							<i className="fas fa-play"></i>
+						</a>
 					</div>
 				</div>		
 	
