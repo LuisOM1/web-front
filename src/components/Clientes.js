4f2428b3a76e7f40ac90d1c043501bd99ebe050c
LUIS <123>
2023-02-10 12:59:30 -0500

    carrusel pending

--- src/components/Clientes.js
+++ src/components/Clientes.js
@@ -6,10 +6,27 @@ import  etours from  '../img/clientes/etours.png';
 import  tjomax from  '../img/clientes/tjomax.png';
 import  ucv from  '../img/clientes/ucv.png';
 
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+
 //Componente funcional Clientes
 function Clientes (){
+    const responsive = {
+        0: { items: 1 },
+        568: { items: 2 },
+        1024: { items: 3 },
+        1200: { items: 5 },
+    };
     
-     
+    const handleDragStart = (e) => e.preventDefault();
+
+    const items = [
+        <img src={cosmos} onDragStart={handleDragStart} role="presentation" alt=''/>,
+        <img src={doil} onDragStart={handleDragStart} role="presentation" alt=''/>,
+        <img src={etours} onDragStart={handleDragStart} role="presentation" alt=''/>,
+        <img src={tjomax} onDragStart={handleDragStart} role="presentation" alt=''/>,
+        <img src={ucv} onDragStart={handleDragStart} role="presentation" alt=''/>,
+    ];
       return (  
         
         <div className='clientes'>
@@ -18,15 +35,18 @@ function Clientes (){
                     <h2>Nuestros clientes nos respaldan</h2>
                 </div>
                 <div className='clientes_sponsors'>      
-                    <ul>
-                        <li><img src={cosmos} alt=" " ></img></li>
-                        <li><img src={doil} alt=" " ></img></li>
-                        <li><img src={etours} alt=" " ></img></li>
-                        <li><img src={tjomax} alt=" " ></img></li>
-                        <li><img src={ucv} alt=" " ></img></li>
-                    </ul>
-
+                    <AliceCarousel
+                        infinite
+                        autoPlay={true}
+                        autoPlayInterval={1000}
+                        disableButtonsControls={true}
+                        disableDotsControls={true}
+                        mouseTracking
+                        items={items}
+                        responsive={responsive}
+                    />
                 </div>
+                
             </div>
         </div>
   
