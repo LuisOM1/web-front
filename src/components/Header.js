654ba58f7d3a47dcfdc3044aaef949272c220e44
LUIS <123>
2023-02-16 11:29:00 -0500

    header-text bold

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,43 +21,43 @@ function Header(){
          
       return  (  <div>
                     <div className="head-superior">
-                    <div className="container-left">
-                    <ul>
-                        <li>
-                        <i className="far fa-envelope-open" aria-hidden="true"></i> contacto@gps.com
-                        </li>
-                        <li> 
-                        <i className="fab fa-whatsapp" aria-hidden="true"></i> +51 970 951 302
-                        </li>
-                        <li> 
-                        <i className="fas fa-phone" aria-hidden="true"></i> (01) 730 4000
-                        </li>
-                    </ul>
+                        <div className="container-left">
+                            <ul>
+                                <li>
+                                    <i className="far fa-envelope-open" aria-hidden="true"></i> contacto@gps.com
+                                </li>
+                                <li> 
+                                    <i className="fab fa-whatsapp" aria-hidden="true"></i> +51 970 951 302
+                                </li>
+                                <li> 
+                                    <i className="fas fa-phone" aria-hidden="true"></i> (01) 730 4000
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div className="container-right">
+                            <ul>
+                                <li className='headersup_horario'> 
+                                    <i className="far fa-clock"></i> Atención: 9:00 AM – 7:00 PM
+                                </li>
+                                <li className='headersup_social'> 
+                                    <a href="https://www.facebook.com/SKIESGPS" target="_blank" className=" icon icon--facebook" rel="noreferrer"> 
+                                        <i className="fab fa-facebook-f"></i> 
+                                    </a>
+                                </li>
+                                <li className='headersup_social'> 
+                                    <a href="https://www.instagram.com/gps_skies" target="_blank" className="icon icon--instagram" rel="noreferrer"> 
+                                        <i className="fab fa-instagram"></i> 
+                                    </a>
+                                </li>
+                                <li className='headersup_social'> 
+                                    <a href="https://twitter.com/gps_skies" target="_blank" className="icon icon--twitter" rel="noreferrer">
+                                        <i className="fab fa-twitter"></i> 
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                    <div className="container-right">
-                    <div className="info">
-                        <ul>
-                        <li> <i className="far fa-clock"></i> Atención: 9:00 AM – 7:00 PM
-                        </li>
-                        <li> 
-                            <a href="https://www.facebook.com/SKIESGPS" target="_blank" className=" icon icon--facebook" rel="noreferrer"> 
-                                <i className="fab fa-facebook-f"></i> 
-                            </a>
-                        </li>
-                        <li> 
-                            <a href="https://www.instagram.com/gps_skies" target="_blank" className="icon icon--instagram" rel="noreferrer"> 
-                                <i className="fab fa-instagram"></i> 
-                            </a>
-                        </li>
-                        <li> 
-                            <a href="https://twitter.com/gps_skies" target="_blank" className="icon icon--twitter" rel="noreferrer">
-                            <i className="fab fa-twitter"></i> 
-                            </a>
-                        </li>
-                        </ul>
-                    </div>            
-                    </div>
-                </div>
 
 
                 <header>
