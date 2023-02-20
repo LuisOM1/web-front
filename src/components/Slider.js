644f4d2091eadbda6cb5ab21efc1c8494f0d40d4
LUIS <123>
2023-02-20 12:36:30 -0500

    Form sendWsp

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../css/slider.css';
 
 
+//Componente funcional Slider
+function Slider() {
+   const [name, setName] = useState('');
+   const [ruc, setRuc] = useState('');
+   const [tel, setTel] = useState('');
 
+   const url_mensaje = `https://api.whatsapp.com/send?phone=+51902534606&text=
+		Yo ${name} con numero de RUC: ${ruc} y teléfono: ${tel} deseo más información`;
 
-//Componente de clase Slider
-class Slider extends React.Component {
-   
-    render(){
-      return    <div className="slider"> 
+   function sendWsp(e){
+        window.open(url_mensaje);
+        e.preventDefault();
+        setName('');
+        setRuc('');
+        setTel('');
+   }
+
+      return (    <div className="slider"> 
                     <div className="slider_container">
                         
                         <div className="slider_container_left">
@@ -31,37 +42,40 @@ class Slider extends React.Component {
                             </div>
                             
                             <div role="form" className="wpcf7" id="guardarform" lang="es-PE" dir="ltr">
-                                <form action="/#guardarform" method="post" className=""  data-status="init">
-                        
-                                <div className="resto-form">
-                                    <div className="col">
-                                        <div className="form-group">
-                                            <span className="input-control-wrap">
-                                                <input type="text" name="nombre" size="40" className="input-form-control" 
-                                                id="nombre" aria-required="true" aria-invalid="false" placeholder="Nombre*" required/>
-                                            </span>
+                                
+                                <form onSubmit={sendWsp}>
+                                    <div className="resto-form">
+                                        <div className="col">
+                                            <div className="form-group">
+                                                <span className="input-control-wrap">
+                                                    <input type="text" name="nombre" size="40" className="input-form-control" 
+                                                    id="nombre" aria-required="true" aria-invalid="false" placeholder="Nombre*" 
+                                                    value={name} onChange={(e) => setName(e.target.value)} required/>
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <div className="col">
+                                            <div className="form-group">
+                                                <span className="input-control-wrap">
+                                                    <input type="text" name="ruc" size="40" className="input-form-control" 
+                                                    id="ruc" aria-required="true" aria-invalid="false" placeholder="RUC*" 
+                                                    value={ruc} onChange={(e) => setRuc(e.target.value)} required/>
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <div className="col">
+                                            <div className="form-group">
+                                                <span className="input-control-wrap">
+                                                    <input type="text" name="telefono" size="40" className="input-form-control"
+                                                    id="telefono" aria-required="true" aria-invalid="false" placeholder="Teléfono*" 
+                                                    value={tel} onChange={(e) => setTel(e.target.value)} required/>
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <div className="col"> 
+                                        <button type="submit" name="submit">Solicitar Información</button>
                                         </div>
                                     </div>
-                                    <div className="col">
-                                        <div className="form-group">
-                                            <span className="input-control-wrap">
-                                                <input type="text" name="ruc" size="40" className="input-form-control" 
-                                                id="ruc" aria-required="true" aria-invalid="false" placeholder="RUC*" required/>
-                                            </span>
-                                        </div>
-                                    </div>
-                                    <div className="col">
-                                        <div className="form-group">
-                                            <span className="input-control-wrap">
-                                                <input type="text" name="telefono" size="40" className="input-form-control"
-                                                id="telefono" aria-required="true" aria-invalid="false" placeholder="Teléfono*" required/>
-                                            </span>
-                                        </div>
-                                    </div>
-                                    <div className="col"> 
-                                    <button type="submit" name="submit">Solicitar Información</button>
-                                    </div>
-                                </div>
                                 </form>
                             </div>
                             </div>
@@ -73,7 +87,7 @@ class Slider extends React.Component {
 
 
   
-    }
+   )
   
   }
 
